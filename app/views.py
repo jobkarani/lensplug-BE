984242f427166cf5a3d2c33b666e461657ecdcd3
Job Karani <jobkarani084j@gmail.com>
2023-08-22 10:54:27 +0300

    feat: creates the views for the classes and their categories

--- app/views.py
+++ app/views.py
@@ -23,9 +23,9 @@ class StandardResultsSetPagination(PageNumberPagination):
 
 
 @api_view(['GET',])
-def weddingCakes(request):
+def vehicleLights(request):
     if request.method == "GET":
-        products = WeddingCakes.objects.all()
+        products = VehicleLights.objects.all()
 
         # Set up pagination
         paginator = PageNumberPagination()
@@ -33,20 +33,21 @@ def weddingCakes(request):
         result_page = paginator.paginate_queryset(products, request)
 
         # Serialize the result page
-        serializer = WeddingCakesSerializer(result_page, many=True)
+        serializer = VehicleLightsSerializer(result_page, many=True)
         return Response(serializer.data)
-
+    
 @api_view(['GET'])
-def weddingCakeDetails(request, product_id):
+def getVehicleLightsByCategory(request, vehicle_category_id):
     if request.method == "GET":
-        product= WeddingCakes.objects.filter(id = product_id)
-        serializer = WeddingCakesSerializer(product, many=True)
+        vehicle_lights_category = get_object_or_404(VehicleLightsCategory, id=vehicle_category_id)
+        vehiclelights = VehicleLights.objects.filter(category=vehicle_lights_category)
+        serializer =VehicleLightsCategorySerializer(vehiclelights, many=True)
         return Response(serializer.data)
     
 @api_view(['GET',])
-def babyShowerCakes(request):
+def accessories(request):
     if request.method == "GET":
-        products = BabyShowerCakes.objects.all()
+        products = Accessories.objects.all()
 
         # Set up pagination
         paginator = PageNumberPagination()
@@ -54,20 +55,21 @@ def babyShowerCakes(request):
         result_page = paginator.paginate_queryset(products, request)
 
         # Serialize the result page
-        serializer = BabyShowerCakesSerializer(result_page, many=True)
+        serializer = AccessoriesSerializer(result_page, many=True)
         return Response(serializer.data)
-
+    
 @api_view(['GET'])
-def babyShowerCakesDetails(request, product_id):
+def getAccessoriesByCategory(request, accessories_category_id):
     if request.method == "GET":
-        product= BabyShowerCakes.objects.filter(id = product_id)
-        serializer = BabyShowerCakesSerializer(product, many=True)
+        accessories_category = get_object_or_404(AccessoriesCategory, id=accessories_category_id)
+        accessories = Accessories.objects.filter(category=accessories_category)
+        serializer =AccessoriesCategorySerializer(accessories, many=True)
         return Response(serializer.data)
     
 @api_view(['GET',])
-def bridalShowerCakes(request):
+def bulbs(request):
     if request.method == "GET":
-        products = BridalShowerCakes.objects.all()
+        products = Bulbs.objects.all()
 
         # Set up pagination
         paginator = PageNumberPagination()
@@ -75,20 +77,21 @@ def bridalShowerCakes(request):
         result_page = paginator.paginate_queryset(products, request)
 
         # Serialize the result page
-        serializer = BridalShowerCakesSerializer(result_page, many=True)
+        serializer = BulbsSerializer(result_page, many=True)
         return Response(serializer.data)
-
+    
 @api_view(['GET'])
-def bridalShowerCakesDetails(request, product_id):
+def getBulbsByCategory(request, bulbs_category_id):
     if request.method == "GET":
-        product= BridalShowerCakes.objects.filter(id = product_id)
-        serializer = BridalShowerCakesSerializer(product, many=True)
+        bulbs_category = get_object_or_404(BulbsCategory, id=bulbs_category_id)
+        bulbs = Bulbs.objects.filter(category=bulbs_category)
+        serializer =BulbsCategorySerializer(bulbs, many=True)
         return Response(serializer.data)
     
 @api_view(['GET',])
-def christmasCakes(request):
+def grilles(request):
     if request.method == "GET":
-        products = ChristmasCakes.objects.all()
+        products = Grilles.objects.all()
 
         # Set up pagination
         paginator = PageNumberPagination()
@@ -96,20 +99,21 @@ def christmasCakes(request):
         result_page = paginator.paginate_queryset(products, request)
 
         # Serialize the result page
-        serializer = ChristmasCakesSerializer(result_page, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def christmasCakesDetails(request, product_id):
-    if request.method == "GET":
-        product= ChristmasCakes.objects.filter(id = product_id)
-        serializer = ChristmasCakesSerializer(product, many=True)
+        serializer = GrillesSerializer(result_page, many=True)
         return Response(serializer.data)
     
-@api_view(['GET',])
-def easterCakes(request):
+@api_view(['GET'])
+def getGrillesByCategory(request, grilles_category_id):
     if request.method == "GET":
-        products = EasterCakes.objects.all()
+        grilles_category = get_object_or_404(GrillesCategory, id=grilles_category_id)
+        grilles = Grilles.objects.filter(category=grilles_category)
+        serializer =GrillesCategorySerializer(grilles, many=True)
+        return Response(serializer.data)
+
+@api_view(['GET',])
+def bumperAndParts(request):
+    if request.method == "GET":
+        products = BumperAndParts.objects.all()
 
         # Set up pagination
         paginator = PageNumberPagination()
@@ -117,274 +121,13 @@ def easterCakes(request):
         result_page = paginator.paginate_queryset(products, request)
 
         # Serialize the result page
-        serializer = EasterCakesSerializer(result_page, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def easterCakesDetails(request, product_id):
-    if request.method == "GET":
-        product= EasterCakes.objects.filter(id = product_id)
-        serializer = EasterCakesSerializer(product, many=True)
-        return Response(serializer.data)
-    
-@api_view(['GET',])
-def fathersDayCakes(request):
-    if request.method == "GET":
-        products = FathersDayCakes.objects.all()
-
-        # Set up pagination
-        paginator = PageNumberPagination()
-        paginator.page_size = 300
-        result_page = paginator.paginate_queryset(products, request)
-
-        # Serialize the result page
-        serializer = FathersDayCakesSerializer(result_page, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def fathersDayCakesDetails(request, product_id):
-    if request.method == "GET":
-        product= FathersDayCakes.objects.filter(id = product_id)
-        serializer = FathersDayCakesSerializer(product, many=True)
-        return Response(serializer.data)
-    
-@api_view(['GET',])
-def mothersDayCakes(request):
-    if request.method == "GET":
-        products = MothersDayCakes.objects.all()
-
-        # Set up pagination
-        paginator = PageNumberPagination()
-        paginator.page_size = 300
-        result_page = paginator.paginate_queryset(products, request)
-
-        # Serialize the result page
-        serializer = MothersDayCakesSerializer(result_page, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def mothersDayCakesDetails(request, product_id):
-    if request.method == "GET":
-        product= MothersDayCakes.objects.filter(id = product_id)
-        serializer = MothersDayCakesSerializer(product, many=True)
-        return Response(serializer.data)
-    
-@api_view(['GET',])
-def graduationCakes(request):
-    if request.method == "GET":
-        products = GraduationCakes.objects.all()
-
-        # Set up pagination
-        paginator = PageNumberPagination()
-        paginator.page_size = 300
-        result_page = paginator.paginate_queryset(products, request)
-
-        # Serialize the result page
-        serializer = GraduationCakesSerializer(result_page, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def graduationCakesDetails(request, product_id):
-    if request.method == "GET":
-        product= GraduationCakes.objects.filter(id = product_id)
-        serializer = GraduationCakesSerializer(product, many=True)
-        return Response(serializer.data)
-    
-@api_view(['GET',])
-def valentinesCakes(request):
-    if request.method == "GET":
-        products = ValentinesCakes.objects.all()
-
-        # Set up pagination
-        paginator = PageNumberPagination()
-        paginator.page_size = 300
-        result_page = paginator.paginate_queryset(products, request)
-
-        # Serialize the result page
-        serializer = ValentinesCakesSerializer(result_page, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def valentinesCakesDetails(request, product_id):
-    if request.method == "GET":
-        product= ValentinesCakes.objects.filter(id = product_id)
-        serializer = ValentinesCakesSerializer(product, many=True)
-        return Response(serializer.data)
-    
-@api_view(['GET',])
-def ruracioCakes(request):
-    if request.method == "GET":
-        products = RuracioCakes.objects.all()
-
-        # Set up pagination
-        paginator = PageNumberPagination()
-        paginator.page_size = 300
-        result_page = paginator.paginate_queryset(products, request)
-
-        # Serialize the result page
-        serializer = RuracioCakesSerializer(result_page, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def ruracioCakesDetails(request, product_id):
-    if request.method == "GET":
-        product= RuracioCakes.objects.filter(id = product_id)
-        serializer = RuracioCakesSerializer(product, many=True)
-        return Response(serializer.data)
-    
-@api_view(['GET',])
-def engagementCakes(request):
-    if request.method == "GET":
-        products = EngagementCakes.objects.all()
-
-        # Set up pagination
-        paginator = PageNumberPagination()
-        paginator.page_size = 300
-        result_page = paginator.paginate_queryset(products, request)
-
-        # Serialize the result page
-        serializer = EngagementCakesSerializer(result_page, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def engagementCakesDetails(request, product_id):
-    if request.method == "GET":
-        product= EngagementCakes.objects.filter(id = product_id)
-        serializer = EngagementCakesSerializer(product, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET',])
-def coporateCakes(request):
-    if request.method == "GET":
-        products = CoporateCakes.objects.all()
-
-        # Set up pagination
-        paginator = PageNumberPagination()
-        paginator.page_size = 300
-        result_page = paginator.paginate_queryset(products, request)
-
-        # Serialize the result page
-        serializer = CoporateCakesSerializer(result_page, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def coporateCakesDetails(request, product_id):
-    if request.method == "GET":
-        product= CoporateCakes.objects.filter(id = product_id)
-        serializer = CoporateCakesSerializer(product, many=True)
-        return Response(serializer.data)
-    
-@api_view(['GET',])
-def anniversaryCakes(request):
-    if request.method == "GET":
-        products = AnniversaryCakes.objects.all()
-
-        # Set up pagination
-        paginator = PageNumberPagination()
-        paginator.page_size = 300
-        result_page = paginator.paginate_queryset(products, request)
-
-        # Serialize the result page
-        serializer = AnniversaryCakesSerializer(result_page, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def anniversaryCakesDetails(request, product_id):
-    if request.method == "GET":
-        product= AnniversaryCakes.objects.filter(id = product_id)
-        serializer = AnniversaryCakesSerializer(product, many=True)
-        return Response(serializer.data)
-    
-@api_view(['GET',])
-def baptismalCakes(request):
-    if request.method == "GET":
-        products = BaptismalCakes.objects.all()
-
-        # Set up pagination
-        paginator = PageNumberPagination()
-        paginator.page_size = 300
-        result_page = paginator.paginate_queryset(products, request)
-
-        # Serialize the result page
-        serializer = BaptismalCakesSerializer(result_page, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def baptismalCakesDetails(request, product_id):
-    if request.method == "GET":
-        product= BaptismalCakes.objects.filter(id = product_id)
-        serializer = BaptismalCakesSerializer(product, many=True)
-        return Response(serializer.data)
-    
-@api_view(['GET',])
-def christeningCakes(request):
-    if request.method == "GET":
-        products = ChristeningCakes.objects.all()
-
-        # Set up pagination
-        paginator = PageNumberPagination()
-        paginator.page_size = 300
-        result_page = paginator.paginate_queryset(products, request)
-
-        # Serialize the result page
-        serializer = ChristeningCakesSerializer(result_page, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def christeningCakesDetails(request, product_id):
-    if request.method == "GET":
-        product= ChristeningCakes.objects.filter(id = product_id)
-        serializer = ChristeningCakesSerializer(product, many=True)
-        return Response(serializer.data)
-    
-@api_view(['GET',])
-def birthdayCakes(request):
-    if request.method == "GET":
-        products = BirthdayCakes.objects.all()
-
-        # Set up pagination
-        paginator = PageNumberPagination()
-        paginator.page_size = 300
-        result_page = paginator.paginate_queryset(products, request)
-
-        # Serialize the result page
-        serializer = BirthdayCakesSerializer(result_page, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def birthdayCakesDetails(request, product_id):
-    if request.method == "GET":
-        product= BirthdayCakes.objects.filter(id = product_id)
-        serializer = BirthdayCakesSerializer(product, many=True)
-        return Response(serializer.data)
-
-
-
-@api_view(['GET'])
-def get_blogs(request):
-    if request.method == "GET":
-        blogs = Blogs.objects.all()
-        serializer = BlogsSerializer(blogs, many=True)
-        return Response(serializer.data)
-    
-@api_view(['GET',])
-def api_blogscategories(request):
-    if request.method == "GET":
-        blogcategories = BlogCategory.objects.all()
-        serializer = BlogCategorySerializer(blogcategories, many=True)
-        return Response(serializer.data)
-
-@api_view(['GET'])
-def getBlogDetails(request, blog_id):
-    if request.method == "GET":
-        blogs= Blogs.objects.filter(id = blog_id)
-        serializer = BlogsSerializer(blogs, many=True)
+        serializer = BumperAndPartsSerializer(result_page, many=True)
         return Response(serializer.data)
     
 @api_view(['GET'])
-def getBlogByCategory(request, blogcategory_id):
+def getBumperAndPartsByCategory(request, bumper_category_id):
     if request.method == "GET":
-        blogcategory = get_object_or_404(BlogCategory, id=blogcategory_id)
-        blogs = Blogs.objects.filter(blog_category=blogcategory)
-        serializer =BlogsSerializer(blogs, many=True)
+        bumper_category = get_object_or_404(BumperAndPartsCategory, id=bumper_category_id)
+        bumper = BumperAndParts.objects.filter(category=bumper_category)
+        serializer =BumperAndPartsCategorySerializer(bumper, many=True)
         return Response(serializer.data)
